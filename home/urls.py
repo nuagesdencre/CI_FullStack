aba82ed452bf8801472555a70fafce9d03ee8607
Unknown <vero@nuagesdencre.com>
2018-11-21 13:24:41 +0000

    created basic views & templates for home app; linked staticfiles img
    tested images displayed

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, contact, about, faq
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact', contact, name='contact'),
+    path('about', about, name='about'),
+    path('faq', faq, name='faq'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,4 +6,28 @@ def index(request):
     Return the index page of the home app
     (this is the welcome page of the app)
     """
-    return render(request, 'index.html')
+    test = {'test': 'This is a test.'}
+    return render(request, 'index.html', context=test)
+
+
+def contact(request):
+    """
+    Return the contact page of the home app
+    """
+    return render(request, 'contact.html')
+
+
+def about(request):
+    """
+    Return the about page of the home app
+    """
+    return render(request, 'about.html')
+
+
+def faq(request):
+    """
+    Return the FAQs page of the home app
+    """
+    faqs = {'Fact_1': 'This is a test.', 'Fact_2': 'This is a test too.',
+            'Fact_3': 'This is also a test.'}
+    return render(request, 'faq.html', context=faqs)
