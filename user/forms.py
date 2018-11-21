5cd3117cfc588c0a0d3f904ab0a96724fe3a1431
Unknown <vero@nuagesdencre.com>
2018-11-21 16:39:31 +0000

    added registration form and included django-forms_bootstrap

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class UserLoginForm(forms.Form):
@@ -8,4 +9,13 @@ class UserLoginForm(forms.Form):
     """
     username= forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-    
+
+
+class RegistrationForm(UserCreationForm):
+    """
+    Where visitors can register to access user-specific features of the website
+    """
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'psw', 'check_psw']
