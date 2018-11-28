14f0e3b38f76e42b225253d249fa5db6262360ee
Unknown <vero@nuagesdencre.com>
2018-11-28 10:55:29 +0000

    reverted to one password at registration - validation to be added

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,19 +8,15 @@ class UserForm(forms.ModelForm):
     Form through which visitors can register their details to access user-specific
     features of the website
     """
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label='Password Confirmation',
-        widget=forms.PasswordInput
-    )
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
+        fields = ['email', 'username', 'password']
 
 
 class UserProfile(forms.ModelForm):
-    profile_img = forms.ImageField(label="Profile Image")
     class Meta:
         model = ProfileModel
         fields = ("community_page", "twitter_handle", "profile_img")
