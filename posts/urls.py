2a48641d2455584a290ed620f2156cab4d8f30e2
Unknown <vero@nuagesdencre.com>
2018-11-29 21:58:10 +0000

    I updated the view name 'user_posts' and made sure that templates match

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='all'),
     path('new', views.CreatePost.as_view(), name='create'),
-    path('auth/<username>', views.UserPosts.as_view(), name='for_user'),
+    path('auth/<username>', views.UserPosts.as_view(), name='user_posts'),
     path('auth/<username>/<pk>', views.PostDetail.as_view(), name='single'),
     path('delete/<pk>', views.DeletePost.as_view(), name='delete'),
 ]
