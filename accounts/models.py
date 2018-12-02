a95c9f02465fcf6390f232c956a20ae816449c45
Unknown <vero@nuagesdencre.com>
2018-12-02 13:58:19 +0000

    I merge my initial profile template with the user_post_list template.
    I also linked the navigation bar to the user_post_list as it stands as the profile of the logged in user.

--- accounts/models.py
+++ accounts/models.py
@@ -5,9 +5,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     # EXTEND USER ATTRIBUTES BY EXTENDING THE USER CLASS
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # COMMUNITY PAGE
     twitter_handle = models.CharField(max_length=100, blank=True)
-    community_page = models.URLField(blank=True)
+    website = models.URLField(blank=True)
     profile_image = models.ImageField(upload_to='profile_imgs', blank=True)
 
     def __str__(self):
