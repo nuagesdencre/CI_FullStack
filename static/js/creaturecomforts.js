9fdcc9a6972fa4a1691049a0bda5dce64bc3824f
Unknown <vero@nuagesdencre.com>
2018-12-10 15:03:39 +0000

    I fixed the alert and added possibility to dismiss it.

--- static/js/creaturecomforts.js
+++ static/js/creaturecomforts.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     $('#menu_button').click(function () {
         $("svg", this).toggleClass("fa-ellipsis-v").toggleClass("fa-times");
     });
-
+    $('.alert').alert()
 });
