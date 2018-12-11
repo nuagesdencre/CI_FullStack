917204f7f01d348dfc6aa7d50f2bbc5ea5b51e31
Unknown <vero@nuagesdencre.com>
2018-12-11 12:50:25 +0000

    I added a special code upon registration.
    I also improved the UX/UI of the login and register templates.

--- static/js/creaturecomforts.js
+++ static/js/creaturecomforts.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
+    /* Toggling different svgs when collapsing/viewing the small screen navbar*/
     $('#menu_button').click(function () {
         $("svg", this).toggleClass("fa-ellipsis-v").toggleClass("fa-times");
     });
-    $('.alert').alert()
+    /* Enabling alert features */
+    $('.alert').alert();
+
+    /* Create a random code for newly registered users (discount code)*/
+    function randomCode(len) {
+        var random_code = "";
+        var ranNum = "0123456789abcdefghijklmnopqrstuvwxyz";
+        for (var i = 0; i < len; i++) {
+            random_code += ranNum.charAt(Math.floor(Math.random() * ranNum.length));
+        }
+        return random_code;
+    }
+
+    random_code = randomCode(12);
+
+    $("#btn_code").click(function () {
+        $("#code_output").html(random_code);
+        $("#btn_code").fadeOut("slow");
+    });
 });
