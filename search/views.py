01cee62da01e7bbfdbfb90425ccc551ef75329f6
Unknown <vero@nuagesdencre.com>
2018-12-11 22:30:59 +0000

    I added the description in the search product filter.

--- search/views.py
+++ search/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from products.models import Product
+from django.db.models import Q
+
 
 def search_index(request):
     """
     Search within the Products
     """
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    query =None
+    products = None
+
+    if 'q' in request.GET:
+        query = request.GET.get('q')
+        products = Product.objects.all().filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     return render(request, "products.html", {"products": products})
