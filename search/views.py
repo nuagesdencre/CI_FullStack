eafc207dc264d42a28561d902317dd221f0ec519
Unknown <vero@nuagesdencre.com>
2018-12-11 23:09:38 +0000

    I added a link to all products view after a search.

--- search/views.py
+++ search/views.py
@@ -3,15 +3,17 @@ from products.models import Product
 from django.db.models import Q
 
 
+
 def search_index(request):
     """
     Search within the Products
     """
-    query =None
+    query = None
     products = None
 
     if 'q' in request.GET:
         query = request.GET.get('q')
         products = Product.objects.all().filter(Q(name__icontains=query) | Q(description__icontains=query))
-
-    return render(request, "products.html", {"products": products})
+        message = "<a href='/products/'><p class='alert alert-info my-3'>View all products here.</p></a>"
+        
+    return render(request, "products.html", {"products": products, "message": message})
