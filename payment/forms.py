9a915dfa2989c901864d9e3ebbad0cf64b4b627a
Unknown <vero@nuagesdencre.com>
2018-12-12 12:35:07 +0000

    I reordered the checkout form fields & updated the disclaimer's styles.

--- payment/forms.py
+++ payment/forms.py
@@ -3,7 +3,7 @@ from .models import Order
 
 
 class MakePaymentForm(forms.Form):
-    MONTH_CHOICES = [(i, i) for i in range(1, 12+1)]
+    MONTH_CHOICES = [(i, i) for i in range(1, 12 + 1)]
     YEAR_CHOICES = [(i, i) for i in range(2018, 2036)]
 
     credit_card_number = forms.CharField(label='Credit card number', required=False)
@@ -17,5 +17,5 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-        'full_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2',
-        'county')
+            'full_name', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode',
+            'county', 'country')
